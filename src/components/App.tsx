b17442f5f4ebbb691c0894aad70d60398ab8d223
Benoitdo <benoit.dossoine@student.ehb.be>
2023-05-03 17:33:21 +0200

    feat(frame): Adds frame of the game

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import Frame from "./Frame";
 
 function App() {
   return (
     <div className="App">
+        <Frame></Frame>
     </div>
   );
 }
---
+++ src/components/Frame.tsx
@@ -0,0 +1,11 @@
+function Frame(){
+  return(
+    <div className="frame-wrapper">
+      <div className="frame">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Frame;
