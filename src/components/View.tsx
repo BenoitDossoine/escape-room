e737f77b7194d4f04e2efc009feb396e328fc11c
Benoitdo <benoit.dossoine@student.ehb.be>
2023-05-12 11:10:49 +0200

    feat(centering): Start in center of room

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,27 +1,16 @@
-import { FirstPersonControls, Center } from '@react-three/drei';
+import { Center } from '@react-three/drei';
 import { FlyControls } from '../controls/react-three-fiber/FlyControls';
 import {Canvas, useLoader} from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 function View(){
-
     const model = useLoader(GLTFLoader, './models/room2.glb');
-    console.log(model);
     return(
-        <Canvas>
-            {/* <FirstPersonControls
-                enabled={true}
-                activeLook={true}
-                movementSpeed={0}
-                lookSpeed={0.1}
-                // constrainVertical={true}
-                // verticalMin={Math.PI}
-                // verticalMax={Math.PI/2}
-            ></FirstPersonControls> */}
+        <Canvas camera={{position: [0,0,0]}}>
             <FlyControls/>
             <Center>
                 <ambientLight intensity={ 0.4 } />
-                <primitive object={model.scene} scale={1.5} position-y={-0.7} position-z={12} position-x={4} rotation-y={-Math.PI/2}/>    
+                <primitive object={model.scene} scale={1} rotation-y={-Math.PI/2}/>    
             </Center>
         </Canvas>
     )
