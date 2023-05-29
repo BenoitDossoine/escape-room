6954bed4c2d25a87ed2f866e5fe2f524bf1198bc
Benoitdo <benoit.dossoine@student.ehb.be>
2023-05-29 15:29:56 +0200

    feat(fullscreen): Adds fullscreen support

---
+++ src/services/ImmersionService.tsx
@@ -0,0 +1,21 @@
+class ImmersionService{
+    public playBackgroundAudio = () => {
+        let audio = document.getElementById("backgroundMusic") as HTMLAudioElement;
+        // audio.play();
+        audio.volume = 0.5;
+    }
+    
+    public pauseBackgroundAudio = () => {
+        let audio = document.getElementById("backgroundMusic") as HTMLAudioElement;
+        audio.pause();
+    }
+
+    public setFullScreen = () => {
+        const body = document.getElementsByTagName("body")[0] as HTMLBodyElement;
+        if(body.requestFullscreen){
+            body.requestFullscreen();
+        }
+    }
+}
+
+export const immersionService = new ImmersionService();
